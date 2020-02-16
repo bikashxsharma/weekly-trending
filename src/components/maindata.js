5872ec1a0650a42fdbd80781ee544bf5f43dbe9e
Bikash Sharma-home-pc <bikashxsharma@gmail.com>
2020-02-16 19:18:58 +0200

    API fetching, sorting function, thousand seperator and HTML/CSS structure done

--- src/components/maindata.js
+++ src/components/maindata.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import star from '../assets/star.svg'
 
 class Maindata extends React.Component{
             constructor(props){
@@ -10,6 +11,8 @@ class Maindata extends React.Component{
                         repos:[],
                         sortType:''
                     }
+
+                    this.handleChange=this.handleChange.bind(this);
 
             }
     /*Fetching data from URL*/     
@@ -24,25 +27,75 @@ class Maindata extends React.Component{
             })
         }).catch(err=>console.log(err));
 
-    }        
+    } 
+    /*Function to sort data*/ 
+    getSortedData(){
+        let sortedRepos;
+        if(this.state.sortType===''){
+            sortedRepos=this.state.repos;
+
+        } else if(this.state.sortType==='byLang'){
+            sortedRepos= this.state.repos.sort((a,b)=>a.language > b.language ? 1 : -1);
+                
+        } else if(this.state.sortType==='byStars'){
+            sortedRepos= this.state.repos.sort((a,b)=>a.currentPeriodStars < b.currentPeriodStars ? 1 : -1);
+           
+        }
+        return sortedRepos;
+    }
+    /*handleChange will change the sortType state according to user selection*/ 
+    handleChange(e){
+        this.setState({sortType:e.target.value});
+    }
+    
 
     render(){
-        const{isLoaded, repos}=this.state;
+        const{isLoaded}=this.state;
+        const sortedRepos = this.getSortedData();
         if(!isLoaded){
             return(<div>Loading....</div>);
         }else{
             return(
-                <div>
-                    <h2>Fresh from the press.</h2>
-                    {
-                        repos.map((repo,id)=>(
-                            <div key={id} >
-                                {repo.author}
-                            </div>
-                        ))
-                    }
+                <div className="main-contents">
+                <h2 className="main-content-title">Fresh from the press.</h2>
 
-                </div>
+            {/** Sorting dropdown  */}
+              <div className="sort-filter">
+                    <div className="sort-select">
+                        <p className="grey">sort by  </p>
+                        <select defaultValue="byStars" onChange={this.handleChange} value={this.state.value} >
+                            <option value="byStars">stars</option>
+                            <option value="byLang">language</option>
+
+                        </select>
+                     
+                    </div> 
+               </div> 
+               
+                
+                {
+                    
+                    sortedRepos.map((repo, id) =>(
+                    <div key={id} className="data-list">
+                        <div className="list-left">
+                        <h2 ><a href={repo.url} target="_blank"><span className="regular">{repo.author}/</span>{repo.name}</a></h2>
+                        <p className="black">{repo.description}</p>
+                        <p className="grey">{repo.language}. {repo.stars.toLocaleString()}</p>
+
+                        </div>
+                        <div className="list-right">
+                        <img src={star} alt="star" /><p> {repo.currentPeriodStars.toLocaleString()} stars this week</p>
+                        </div>    
+                    
+                   
+                   
+                    </div>
+                    ))
+
+                }
+
+                <p className="seemore"><a href="http://github.com/trending" target="_blank">See more in Github</a></p>
+            </div>
             );
            
             
